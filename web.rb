db132ccdbeca3891aafb856139a6e9d3800c58c0
Bruce Steedman <bruce.steedman@gmail.com>
2014-10-07 11:47:04 +0100

    Replace ActiveRecord and PG with mongo, configure db connection

--- web.rb
+++ web.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
-require 'sinatra/activerecord'
-require './config/environments' # db config
+require 'mongo'
+
+include Mongo
+
+configure do
+  mongo_url = ENV['MONGOLAB_URI'] || 'mongodb://localhost:27017/pride-development'
+  uri = URI.parse(mongo_url)
+  db_name = uri.path.gsub('/', '')
+  conn = MongoClient.from_uri(mongo_url)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db(db_name)
+end
 
 get '/' do
-  'Hello World'
+  settings.mongo_db.name
 end
