0897ee419e7df1d9b325bcdbd22ff271335d6c59
Bruce Steedman <bruce.steedman@gmail.com>
2014-10-10 14:29:56 +0100

    Add gem; better_errors, add routes to list all docs and a list a doc by name and add a new doc

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 ruby "2.1.2"
 
 gem 'sinatra', '1.4.5'
+gem 'better_errors', '2.0.0'
 gem 'shotgun', '0.9'
 gem 'rspec', '3.1.0'
 gem 'unicorn', '4.8.3'
--- web.rb
+++ web.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'better_errors'
 require 'mongo'
+require 'json/ext' # required for .to_json
 
 include Mongo
 
@@ -10,8 +12,41 @@ configure do
   conn = MongoClient.from_uri(mongo_url)
   set :mongo_connection, conn
   set :mongo_db, conn.db(db_name)
+  set :docs, Proc.new { mongo_db.collection('docs') } # proc needed here
 end
 
-get '/' do
-  settings.mongo_db.name
+configure do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+helpers do
+  # a helper method to turn a string ID
+  # representation into a BSON::ObjectId
+  def object_id bson_string
+    BSON::ObjectId.from_string(bson_string)
+  end
+
+  def doc_by_name doc_name
+    settings.docs.find_one(name: doc_name).to_json
+  end
+
+  def doc_source doc_name
+    JSON.parse(doc_by_name(doc_name))['source']
+  end
+end
+
+get '/document/:name/?' do
+  doc_source(params[:name])
+end
+
+get '/documents/?' do
+  content_type :json
+  settings.docs.find.to_a.to_json
+end
+
+post '/new_doc' do
+  name = params[:name]
+  source = params[:source]
+  docs.insert(name: name, source: source)
 end
